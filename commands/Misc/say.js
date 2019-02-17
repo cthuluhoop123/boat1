eabccb08fe49583fce42524ae134af39c0725a9b
Joe Smith <srynotsry911@gmail.com>
2019-02-17 15:27:42 +0200

    Update say.js

--- commands/Misc/say.js
+++ commands/Misc/say.js
@@ -5,6 +5,7 @@ module.exports.run = async (bot, message, args) => {
     if(message.author.id != ids.Joe) return message.channel.send(`${emotes.Error} **You don't have permissions to use this command.**`);
     
     let sayMsg = args.join(" ");
+    await message.delete()
     message.channel.send(sayMsg);
 
 }
